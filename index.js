c21d9c3d334d3cd585a082e1054dd9f58fa6dc81
Ilya Kozhevnikov <git@kozhevnikov.com>
2017-07-18 12:43:21 +0100

    Implement parseBegin

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
 const logger = require('jsdoc/util/logger');
+const path = require('path');
 
-logger.debug('[JMN] Loading plugin jsdoc-memberof-namespace');
+logger.debug('[JMN] Loaded jsdoc-memberof-namespace');
+
+const handlers = {
+  parseBegin: (event) => {
+    const files = event.sourcefiles;
+    const index = [];
+    const other = [];
+
+    files.forEach(file => (path.basename(file) === 'index.js' ? index : other).push(file));
+    logger.debug(`[JMN] Found ${index.length} index.js`, index);
+
+    if (index.length === 0) return;
+
+    files.length = 0;
+    files.push(...index);
+    files.push(...other);
+  },
+
+  fileBegin: (event) => {
+    logger.debug('fileBegin');
+  },
+
+  beforeParse: (event) => {
+    logger.debug('beforeParse');
+  },
+
+  jsdocCommentFound: (event) => {
+    logger.debug('jsdocCommentFound');
+  },
+
+  symbolFound: (event) => {
+    logger.debug('symbolFound');
+  },
+
+  newDoclet: (event) => {
+    logger.debug('newDoclet');
+  },
+
+  fileComplete: (event) => {
+    logger.debug('fileComplete');
+  },
+
+  parseComplete: (event) => {
+    logger.debug('parseComplete');
+  },
+
+  processingComplete: (event) => {
+    logger.debug('processingComplete');
+  }
+};
+
+module.exports = { handlers };
