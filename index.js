21c3f1b46df3943dbc75785fb358de69f38ce541
Ilya Kozhevnikov <git@kozhevnikov.com>
2017-07-25 01:22:49 +0100

    Implement 2.0.0

--- index.js
+++ index.js
@@ -8,57 +8,89 @@ const namespaces = [];
 
 const handlers = {
   parseBegin: (event) => {
-    const files = event.sourcefiles;
-    const index = [];
-    const other = [];
-
-    files.forEach(file => (path.basename(file) === 'index.js' ? index : other).push(file));
-    logger.debug(`[JMN] Found ${index.length} index.js`, index);
-
-    if (index.length === 0) return;
-
-    files.length = 0;
-    files.push(...index);
-    files.push(...other);
+    event.sourcefiles.sort((f1, f2) => isIndex(f1) === isIndex(f2) ? 0 : isIndex(f1) ? -1 : 1);
+    logger.debug('[JMN] Sorted', event.sourcefiles);
   },
 
   newDoclet: (event) => {
-    const doclet = event.doclet;
+    const doc = event.doclet;
+    const dsc = `${doc.kind} ${doc.longname} in ${doc.meta ? doc.meta.filename : '.'}`;
 
-    if (doclet.kind === 'namespace' && doclet.meta.filename === 'index.js') {
-      namespaces.push(doclet);
-      logger.debug(`[JMN] Found ${doclet.name} namespace in ${doclet.meta.path}`);
+    if (doc.kind === 'namespace') {
+      if (namespaces.every(ns => ns.longname !== doc.longname)) {
+        logger.debug(`[JMN] Found ${dsc}`);
+      } else {
+        logger.warn(`[JMN] Found duplicate ${dsc}`);
+      }
+      namespaces.push(doc);
       return;
     }
 
-    const file = doclet.meta ? doclet.meta.filename : '*';
-    const desc = `${doclet.name} ${doclet.kind} in ${file}`;
-
-    if (!kinds.includes(doclet.kind)) {
-      logger.debug(`[JMN] Skipped ${desc}`);
+    if (!kinds.includes(doc.kind)) {
+      logger.debug(`[JMN] Skipped ${dsc}`);
       return;
     }
 
-    if (doclet.memberof) {
-      logger.debug(`[JMN] Skipped ${desc} member of ${doclet.memberof}`);
+    if (doc.memberof) {
+      logger.debug(`[JMN] Skipped ${dsc} member of ${doc.memberof}`);
       return;
     }
 
-    const pathspaces = namespaces.filter(ns => doclet.meta.path.startsWith(ns.meta.path));
+    const namespace = getNamespace(doc.meta.filename, doc.meta.path);
 
-    if (pathspaces.length === 0) {
-      logger.warn(`[JMN] No namespace found for ${desc}`);
+    if (!namespace) {
+      logger.warn(`[JMN] Namespace not found for ${dsc}`);
       return;
     }
 
-    const namespace = pathspaces.sort((ns1, ns2) => ns2.meta.path.length - ns1.meta.path.length)[0];
+    doc.memberof = namespace.longname;
+    doc.longname = `${doc.memberof}.${doc.longname}`;
+    if (doc.scope === 'global') doc.scope = 'static';
 
-    doclet.memberof = namespace.longname;
-    doclet.longname = `${doclet.memberof}.${doclet.longname}`;
-    if (doclet.scope === 'global') doclet.scope = 'static';
-
-    logger.debug(`[JMN] Processed ${desc}`);
+    logger.debug(`[JMN] Processed ${dsc} member of ${doc.memberof}`);
   }
 };
+
+/**
+ * Check if file path is an index.js file
+ * @param {string} file - File path
+ * @return {boolean} True if file is index.js, false otherwise
+ */
+function isIndex(file) {
+  return path.basename(file).toLowerCase() === 'index.js';
+}
+
+/**
+ * Check if directory path contains a subdirectory path
+ * @param {string} parent - Directory path
+ * @param {string} child - Subdirectory path
+ * @return {boolean} - True if subdirectory is in directory, false otherwise
+ */
+function isParent(parent, child) {
+  return !path.relative(parent, child).startsWith('..');
+}
+
+/**
+ * Get closest namespace doclet previously parsed either from the same file as the doclet
+ * or from one of the index.js files present in the same directory or any of its parents
+ *
+ * @param {string} file - File name of the doclet
+ * @param {string} path - Directory path of the doclet
+ * @return {?Doclet} - Namespace doclet or null
+ */
+function getNamespace(file, path) {
+  const files = namespaces.filter(ns => ns.meta.filename === file && ns.meta.path === path);
+  if (files.length > 0) {
+    return files[files.length - 1];
+  }
+
+  const paths = namespaces.filter(ns => isIndex(ns.meta.filename) && isParent(ns.meta.path, path));
+  if (paths.length > 0) {
+    paths.sort((ns1, ns2) => ns1.meta.path.length - ns2.meta.path.length);
+    return paths[paths.length - 1];
+  }
+
+  return null;
+}
 
 module.exports = { handlers };
